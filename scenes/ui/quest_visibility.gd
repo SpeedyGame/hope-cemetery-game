dec8e2d8f372ab8d81da592f58a20fed97eaa214
zvshack <zvshack@knox.edu>
2025-04-23 16:36:51 -0500

    visible person meny

--- scenes/ui/quest_visibility.gd
+++ scenes/ui/quest_visibility.gd
@@ -2,10 +2,11 @@ extends Control
 
 @onready var quest = $Panel/VBoxContainer/quest_button
 @onready var quest_gui = $quest_gui
+@onready var people_gui = $people_gui
 
 func _ready():
 	quest_gui.visible = false
-	print("here")
+	people_gui.visible = false
 
 func hide_canvas():
 	if not get_parent().visible:
@@ -22,3 +23,12 @@ func _on_quest_button_pressed():
 		quest_gui.visible = true
 	else:
 		quest_gui.visible = false
+	people_gui.visible = false
+
+
+func _on_people_button_pressed():
+	if people_gui.visible == false:
+		people_gui.visible = true
+	else:
+		people_gui.visible = false
+	quest_gui.visible = false
