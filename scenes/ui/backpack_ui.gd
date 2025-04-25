5ba476fdf8bfb86b71783de441b43e96e8956588
zvshack <zvshack@knox.edu>
2025-04-25 15:32:08 -0500

    adding start of inventory layout

--- scenes/ui/backpack_ui.gd
+++ scenes/ui/backpack_ui.gd
@@ -1,8 +1,10 @@
 extends CanvasLayer
 
 @onready var journal = $journal_gui
+@onready var inventory = $inventory_gui
 @onready var backpack_items = $backpack_buttons
 @onready var journal_control = journal.get_node("Control")
+@onready var inventory_control = inventory.get_node("Control")
 
 
 func _ready():
@@ -25,3 +27,13 @@ func _on_quest_button_pressed():
 	else:
 		journal.visible = false
 		journal_control.hide_canvas()
+
+
+func _on_inventory_button_pressed():
+	if inventory.visible == true:
+		inventory.visible = false
+		inventory_control.hide_canvas()
+	if backpack_items.visible == false:
+		backpack_items.visible = true
+	else:
+		backpack_items.visible = false
