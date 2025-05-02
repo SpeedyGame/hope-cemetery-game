ceda9b415ef964a4b9186608c537ed85e7fb18ee
brij-patel-knox <bpatel@knox.edu>
2025-05-02 15:45:57 -0500

    Inventory Layot
    Items and layout grids.

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,28 +1,24 @@
 extends Control
 
-
-func resume():
-	get_tree().paused = false
-	$AnimationPlayer.play_backwards("blur")
+func _ready():
+	# Start with pause menu hidden
+	self.visible = false
 
 func pause():
 	get_tree().paused = true
-	$AnimationPlayer.play("blur")
+	self.visible = true
 
-func testEsc():
-	if Input.is_action_just_pressed("esc") and !get_tree().paused:
-		pause()
-	elif Input.is_action_just_pressed("esc") and get_tree().paused:
-		resume()
+func resume():
+	get_tree().paused = false
+	self.visible = false
 
+# === BUTTON SIGNALS ===
 
 func _on_resume_pressed():
 	resume()
 
-
 func _on_quit_pressed():
 	get_tree().quit()
-
 
 func _on_options_pressed():
 	resume()
@@ -33,30 +29,28 @@ func _on_main_menu_pressed():
 	get_tree().change_scene_to_file("res://main_menu.tscn")
 
 func _on_save_pressed():
-	
-	print("Game saved!") 
+	print("Game saved!")
 
 func _on_return_to_cemetery_pressed():
 	resume()
-	get_tree().change_scene_to_file("res://cemetery.tscn") 
+	get_tree().change_scene_to_file("res://cemetery.tscn")
 
 func _on_sound_control_pressed():
 	resume()
-	get_tree().change_scene_to_file("res://sound_options_menu.tscn") 
+	get_tree().change_scene_to_file("res://sound_options_menu.tscn")
 
 func _on_quest_menu_pressed():
 	resume()
-	get_tree().change_scene_to_file("res://quest_menu.tscn") 
+	get_tree().change_scene_to_file("res://quest_menu.tscn")
 
 func _on_restart_pressed() -> void:
-	pass 
+	pass
 
 func _on_go_to_main_settings_pressed() -> void:
 	pass
 
 func _on_go_back_to_cemetery_pressed() -> void:
-	pass 
-
+	pass
 
 func _on_settings_pressed() -> void:
 	pass
