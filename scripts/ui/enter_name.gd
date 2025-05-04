6b1d7150e38f010e6e953a678b994b38ffcaff87
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-05-04 15:21:01 -0500

    linked player name to enter_name screen

--- scripts/ui/enter_name.gd
+++ scripts/ui/enter_name.gd
@@ -1,11 +1,13 @@
 extends Control
 
 var speed = 50
+signal name_entered
 
 func _process(delta):
 	$Path2D/PathFollow2D.progress += delta*speed
 
 
 func _on_confirm_button_down():
+	Dialogic.VAR.get('PlayerDescription').set('Name', $Name.text)
 	print(Dialogic.VAR.get('PlayerDescription').get('Name'))
-	
+	name_entered.emit()
