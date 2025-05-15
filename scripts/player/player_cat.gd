1c0cdbc2a8bffa82252d1eac22c72da2d7dc1d5c
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-05-15 15:07:50 -0500

    transferred files over to project

--- scripts/player/player_cat.gd
+++ scripts/player/player_cat.gd
@@ -10,6 +10,7 @@ extends CharacterBody2D
 func _ready():
 	update_animation_parameters(startDir)
 
+
 func _physics_process(_delta):
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
