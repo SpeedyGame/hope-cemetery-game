79c097ee934f11bb1a0dea2bc82c5ef376122161
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-05-17 12:40:34 -0500

    made basic stationary npc

---
+++ scripts/npcs/stationary_npc.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@export var animation_player : Node
+@export var default_animation : String
+
+func _ready():
+	if(animation_player is AnimatedSprite2D):
+		animation_player.play(default_animation)
+
+func switch_animation(new_animation):
+	animation_player.play(new_animation)
