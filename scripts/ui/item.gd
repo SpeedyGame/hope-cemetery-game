a4015936b9924df515d1fc4af21fa44917bd6d45
zvshack <zvshack@knox.edu>
2025-05-26 16:34:38 -0500

    fixing branch issues

--- scripts/ui/item.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _ready():
-	if randi() % 2 == 0:
-		$TextureRect.texture=load("res://Iron Sword.png")
-	else:
-		$TextureRect.texture=load("res://Tree Branch.png")
--- scripts/ui/slot.gd
+++ scripts/ui/slot.gd
@@ -1,9 +1,4 @@
-extends Panel
+extends Control
 
-var ItemClass = preload("res://scenes/ui/item.tscn")
+
 var item = null
-
-func _ready():
-	if randi() % 2 == 0:
-		item = ItemClass.instantiate()
-		add_child(item)
