7186754f92e4870c72538b1b358a60de7e8f08c8
Carissa Medlock <carissa.m.medlock@gmail.com>
2025-05-27 20:37:56 -0500

    Added Minimap
    This adds the minimap to the scene!

---
+++ scenes/panel.gd
@@ -0,0 +1,9 @@
+extends Control
+
+@export var target: CharacterBody2D
+@export var map_scale: float = 0.2
+@export var minimap_center: Vector2 = Vector2(1180,67)
+
+func _process(delta):
+	if target:
+		position = minimap_center + target.global_position * map_scale
