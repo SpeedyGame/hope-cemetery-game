727eccb3d726ae17a292be39979b1a86c7f2a9a4
Carissa Medlock <carissa.m.medlock@gmail.com>
2025-05-27 21:21:07 -0500

    Fix Scale
    This fixes the scales issues

--- scenes/panel.gd
+++ scenes/panel.gd
@@ -2,7 +2,7 @@ extends Control
 
 @export var target: CharacterBody2D
 @export var map_scale: float = 0.2
-@export var minimap_center: Vector2 = Vector2(1180,67)
+@export var minimap_center: Vector2 = Vector2(1180,62)
 
 func _process(delta):
 	if target:
